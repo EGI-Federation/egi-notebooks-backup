5f597a351018270a1d7eb78a1c63ee27f349f60e
Enol Fernandez <enol.fernandez@egi.eu>
2019-06-06 07:38:57 +0100

    More and more fixes

--- backup.sh
+++ backup.sh
@@ -3,7 +3,7 @@
 set -x
 
 # produce a list of the PVCs for restoring
-kubectl get pvc --all-namespaces -o yaml > $PV_DIR/pvc
+kubectl get pvc --all-namespaces -o yaml > $NFS_PATH/pvc
 
 # Backup things (not prometheus...)
-restic backup --exclude='*-prom-prometheus-server-pvc-*' $PV_DIR
+restic backup --exclude='*-prom-prometheus-server-pvc-*' $NFS_PATH
